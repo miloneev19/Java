249c92a1c595c9f9dc5fc983f02ca184a5a90bc9
Milonee Vyas <115082114+miloneev19@users.noreply.github.com>
2023-07-17 16:13:43 +0530

    test.java

--- test.java
+++ test.java
@@ -4,13 +4,7 @@ public class test
 {
 public static void main(String [] args)
 {
-   Scanner sc = new Scanner(System.in);
-   System.out.println("Enter First NUmber:");
-   int a = sc.nextInt();
-   System.out.println("Enter Second NUmber:");
-   int b = sc.nextInt();
-   int c= a+b;
-   System.out.println("Sum:" + c);
+   System.out.println("Hello World");
    
 }
 }
