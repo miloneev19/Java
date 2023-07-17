eeb9fedb990348b86345919fa93f4019b61046d3
Milonee Vyas <115082114+miloneev19@users.noreply.github.com>
2023-07-17 15:14:29 +0530

    HelloWorld

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class test
+{
+public static void main(String [] args)
+{
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter First NUmber:");
+   int a = sc.nextInt();
+   System.out.println("Enter Second NUmber:");
+   int b = sc.nextInt();
+   int c= a+b;
+   System.out.println("Sum:" + c);
+   
+}
+}
