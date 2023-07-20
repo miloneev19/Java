e95e35bb4c64af8cb487f94a22a9afa3a61a0149
Milonee Vyas <115082114+miloneev19@users.noreply.github.com>
2023-07-20 17:15:24 +0530

    String Usage

--- hello.java
+++ hello.java
@@ -1,33 +1,25 @@
-import java.math.BigDecimal;
 
 public class hello {
 
-	public static void main(String[] args) {
-		
-		/*
-		double x1 =1.05;
-		double x2= 2.55;
-		System.out.println(x1+x2);  //output comes 3.5999999999999996 
-		//this id due to java's automatic roundoff feature 
-		*/
-		
-		
-		//so we will use BigDecimal class 
-		/*
-		BigDecimal d1 = new BigDecimal(1.05);
-		BigDecimal d2 = new BigDecimal(2.55);
-		//output should be 3.6
-		System.out.println(d1.add(d2));
-  //output comes as 3.5999999999999998667732370449812151491641998291015625
-	
-	*/
-		
-		//convert the int into strings 
-		BigDecimal d1 = new BigDecimal("1.05");
-		BigDecimal d2 = new BigDecimal("2.55");
-		//output should be 3.6
-		System.out.println(d1.add(d2));
-		//output is 3.60 hence issue fixed 
-	}
-
+	   public static void main(String [] args)
+	   {
+		   String var = "Hello World \u002B";
+		   //002B is unicode for + symbol 
+		   System.out.println(var);
+		   
+		   //concatenating
+		   int var1 = 10;
+		   String var2 = "20";
+		   String var3 = "30";
+		   System.out.println(var1+var2);
+		   //output 1020 --> 10 which is of int type is type casted and converted into string
+		   System.out.println(var2+var3);  //output 2030
+		   System.out.println(var1 + 20 +"hi");
+		   //output becomes 30hi
+		   
+		   //proper way of declaring a String 
+		   String var5 = new String ("Proper Way!");
+		   System.out.println(var5);
+	   }
 }
+
