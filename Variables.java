eafad65569f97a7fadb323e0270c5c39d9e16674
Milonee Vyas <115082114+miloneev19@users.noreply.github.com>
2023-07-20 17:18:27 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		 int x1=1;
+		 System.out.println(x1);
+		 String s1="Hello Everyone!";
+		 System.out.println(s1);
+		 int x2 = (2*2)-1;
+		 System.out.println(x2);
+	}
+
+}
+ 
